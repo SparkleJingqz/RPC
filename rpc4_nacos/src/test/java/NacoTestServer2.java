c0c0cd011ac960fefb36b38e34197ff87768c619
Xiaojing <56394610+SparkleJingqz@users.noreply.github.com>
2022-03-23 22:56:02 +0800

    4_nacos

--- rpc4_nacos/src/test/java/NacoTestServer2.java
+++ rpc4_nacos/src/test/java/NacoTestServer2.java
@@ -1,17 +1,18 @@
 import Server.Enumeration.BalancerType;
 import Server.Enumeration.SerializerCode;
 import Server.NettyServer;
-import Server.Serializer.KryoSerializer;
+import rpc_api.Impl.Bye2Impl;
 import rpc_api.Impl.HelloServiceImpl;
+import rpc_api.Interface.ByeService;
 import rpc_api.Interface.HelloService;
 
-public class NacosTestServer {
+public class NacoTestServer2 {
     public static void main(String[] args) {
-        HelloService helloService = new HelloServiceImpl();
-        NettyServer server = new NettyServer("127.0.0.1", 9000);
+        ByeService byeService = new Bye2Impl();
+        NettyServer server = new NettyServer("127.0.0.1", 9999);
         server.setSerializer(SerializerCode.KRYO.getCode());
         server.setBalancer(BalancerType.ROUND.getCode());
-        server.publishService(helloService, HelloService.class);
+        server.publishService(byeService, ByeService.class);
         server.start();
     }
 }
--- rpc4_nacos/src/test/java/NacosTestClient.java
+++ rpc4_nacos/src/test/java/NacosTestClient.java
@@ -3,6 +3,7 @@ import Client.RpcClient;
 import Client.RpcClientProxy;
 import Server.Serializer.KryoSerializer;
 import rpc_api.Impl.HelloObject;
+import rpc_api.Interface.ByeService;
 import rpc_api.Interface.HelloService;
 
 public class NacosTestClient {
@@ -11,7 +12,9 @@ public class NacosTestClient {
         RpcClientProxy rpcClientProxy = new RpcClientProxy(client);
         HelloService helloService = rpcClientProxy.getProxy(HelloService.class);
         HelloObject obj = new HelloObject(122, "Netty Test");
+        ByeService byeService = rpcClientProxy.getProxy(ByeService.class);
         String res = helloService.hello(obj, 200);
-        System.out.println(res);
+        final String bye = byeService.Bye();
+        System.out.println(res + " " + bye);
     }
 }
